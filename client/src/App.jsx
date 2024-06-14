85d0501381749e59927addc89d3f140e8d3d7274
mjs <msadeghian262@gmail.com>
2024-06-14 09:10:56 +0330

    idk

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,20 @@
-import { useState } from 'react'
+import { useEffect, useRef, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const userData = useRef();
+
+  useEffect(() => {
+    fetch("http://127.0.0.1:8000/api/users")
+      .then(res => res.json())
+      .then(data => userData.current = data)
+  } , [])
 
   return (
     <>
-      
+      <h2>{userData}</h2>
     </>
   )
 }
