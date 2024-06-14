c54f145900094e4d500094c70fd7be6dea18b0e5
mjs <msadeghian262@gmail.com>
2024-06-14 08:50:31 +0330

    blank project

--- exercise/settings.py
+++ exercise/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-k$qx4+!rp=(v#5)399ms)-q-ewizyprjcnc&j-fu3j-@1-fyl%'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
